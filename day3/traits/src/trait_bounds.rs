f5590edb6533e54ccc6ae4b1a00db796e258179a
milley <milley.gm@gmail.com>
2023-04-01 21:19:29 +0800

    ch24.3 trait bounds

---
+++ day3/traits/src/trait_bounds.rs
@@ -0,0 +1,26 @@
+// fn duplicate<T: Clone>(a: T) -> (T, T) {
+//     (a.clone(), a.clone())
+// }
+fn duplicate<T>(a: T) -> (T, T)
+where
+    T: Clone,
+{
+    (a.clone(), a.clone())
+}
+
+// syntatic sugar for:
+//   fn add_42_millons<T: Into<i32>>(x: T) -> i32 {}
+fn add_42_millons(x: impl Into<i32>) -> i32 {
+    x.into() + 42_000_000
+}
+
+fn main() {
+    let foo = String::from("foo");
+    let pair = duplicate(foo);
+    println!("{pair:?}");
+
+    let many = add_42_millons(42_i8);
+    println!("{many}");
+    let many_more = add_42_millons(10_000_000);
+    println!("{many_more}");
+}
