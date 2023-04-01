2efac3ec720711d3d224c9ea9edfc00ff9304b81
milley <milley.gm@gmail.com>
2023-04-01 20:12:56 +0800

    ch24.1 trait objects

---
+++ day3/traits/src/trait_objects.rs
@@ -0,0 +1,31 @@
+trait Pet {
+    fn name(&self) -> String;
+}
+
+struct Dog {
+    name: String,
+}
+
+struct Cat;
+
+impl Pet for Dog {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+}
+
+impl Pet for Cat {
+    fn name(&self) -> String {
+        String::from("The cat")
+    }
+}
+
+fn main() {
+    let pets: Vec<Box<dyn Pet>> = vec![
+        Box::new(Dog { name: String::from("Fido") }),
+        Box::new(Cat),
+    ];
+    for pet in pets {
+        println!("Hello {}!", pet.name());
+    }
+}
