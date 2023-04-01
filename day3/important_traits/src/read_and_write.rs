d119d5fb5534dbe627ccad30d49d45e1b0a8482b
milley <milley.gm@gmail.com>
2023-04-01 21:44:21 +0800

    ch25.4 read and write

---
+++ day3/important_traits/src/read_and_write.rs
@@ -0,0 +1,26 @@
+use std::io::{BufRead, BufReader, Read, Result, Write};
+
+fn count_lines<R: Read>(reader: R) -> usize {
+    let buf_reader = BufReader::new(reader);
+    buf_reader.lines().count()
+}
+
+fn log<W: Write>(writer: &mut W, msg: &str) -> Result<()> {
+    writer.write_all(msg.as_bytes())?;
+    writer.write_all("\n".as_bytes())
+}
+
+fn main() -> Result<()> {
+    let slice: &[u8] = b"foo\nbar\nbaz\n";
+    println!("lines in slice: {}", count_lines(slice));
+
+    let file = std::fs::File::open(std::env::current_exe()?)?;
+    println!("lines in file: {}", count_lines(file));
+
+    let mut buffer = Vec::new();
+    log(&mut buffer, "Hello")?;
+    log(&mut buffer, "World")?;
+    println!("Logged: {buffer:?}");
+
+    Ok(())
+}
