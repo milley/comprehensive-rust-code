1affe35e98576ea778ed88b159205f96267ec13b
milley <milley.gm@gmail.com>
2023-04-02 00:53:27 +0800

    ch28 testing

---
+++ day3/28_testing/src/doc_test.rs
@@ -0,0 +1,12 @@
+/// Shortens a string to the given length.
+///
+/// ```
+/// use playground::shorten_string;
+/// assert_eq!(shorten_string("Hello World", 5), "Hello");
+/// assert_eq!(shorten_string("Hello World", 20), "Hello World");
+/// ```
+pub fn shorten_string(s: &str, length: usize) -> &str {
+    &s[..std::cmp::min(length, s.len())]
+}
+
+fn main() {}
---
+++ day3/28_testing/src/unit_test.rs
@@ -0,0 +1,26 @@
+#[allow(dead_code)]
+fn first_word(text: &str) -> &str {
+    match text.find(' ') {
+        Some(idx) => &text[..idx],
+        None => &text,
+    }
+}
+
+#[test]
+fn test_empty() {
+    assert_eq!(first_word(""), "");
+}
+
+#[test]
+fn test_single_word() {
+    assert_eq!(first_word("Hello"), "Hello");
+}
+
+#[test]
+fn test_multiple_word() {
+    assert_eq!(first_word("Hello world"), "Hello");
+}
+
+fn main() {
+    println!("Hello, world!");
+}
