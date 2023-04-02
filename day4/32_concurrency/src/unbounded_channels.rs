dccff6a7c80a20def42ff95aad77027befbbb038
milley <milley.gm@gmail.com>
2023-04-02 18:44:36 +0800

    ch32.3.1 unbounded channels

---
+++ day4/32_concurrency/src/unbounded_channels.rs
@@ -0,0 +1,19 @@
+use std::{sync::mpsc, thread, time::Duration};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let thread_id = thread::current().id();
+        for i in 0..10 {
+            tx.send(format!("Message {i}")).unwrap();
+            println!("{thread_id:?}: sent Message {i}");
+        }
+        println!("{thread_id:?}: done");
+    });
+    thread::sleep(Duration::from_millis(100));
+
+    for msg in rx.iter() {
+        println!("Main: got {}", msg);
+    }
+}
