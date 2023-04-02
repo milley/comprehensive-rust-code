3d7c942ce9bdf1dd531fa8ec59a0a6caf02a2995
milley <milley.gm@gmail.com>
2023-04-02 10:19:04 +0800

    ch29.5 implementing  unsafe trait

--- day3/29_unsafe_rust/src/unsafe_func.rs
+++ day3/29_unsafe_rust/src/unsafe_func.rs
@@ -13,6 +13,20 @@ extern "C" {
     fn abs(input: i32) -> i32;
 }
 
+use std::mem::size_of_val;
+use std::slice;
+/// ...
+/// # Safety
+/// The type must have a defined representation and no padding.
+pub unsafe trait AsBytes {
+    fn as_bytes(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self as *const Self as *const u8, size_of_val(self)) }
+    }
+}
+
+// Safe because u32 has a defined representation and no padding.
+unsafe impl AsBytes for u32 {}
+
 fn main() {
     let emojis = "🗻∈🌏";
 
